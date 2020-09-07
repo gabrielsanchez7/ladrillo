8cfb98a9ff4bd4b7101c41918f3b9c38526bf888
gabrielsanchez7 <gabriel@plazapoints.com>
2020-09-06 22:01:12 -0500

    .

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const preloadingAnimations = () => {
   setTimeout(() => textPreloading.style.animation = 'textPreloading forwards 0.7s', 950)
 }
 
-preloadingAnimations()
+// preloadingAnimations()
 
 const slidesAnimation = () => {
   const slides = document.getElementById('slides')
@@ -34,13 +34,13 @@ const slidesAnimation = () => {
       image2.style.animation = 'imageIn forwards 0.3s cubic-bezier(.5,.1,.5,.9)'
       slides.style.backgroundColor = '#fdfdfd'
       preLoading.style.display = 'none'
-      page.style.display = 'block'
       setTimeout(() => {
         text2.style.animation = 'textOut forwards 0.3s cubic-bezier(.5,.1,.5,.9)'
         text3.style.animation = 'textIn forwards 0.3s cubic-bezier(.5,.1,.5,.9)'
         image3.style.animation = 'imageIn forwards 0.3s cubic-bezier(.5,.1,.5,.9)'
         slides.style.backgroundColor = '#478bd2'
         setTimeout(() => {
+          page.style.display = 'block'
           fullBox.style.top = '100%'
           slides.style.top = '100%'
           image1.style.animation = 'imageOut forwards 0.3s cubic-bezier(.5,.1,.5,.9)'
@@ -54,7 +54,7 @@ const slidesAnimation = () => {
   }, 2000)
 }
 
-slidesAnimation()
+// slidesAnimation()
 
 const bannersAnimation = () => {
 
@@ -68,7 +68,6 @@ const bannersAnimation = () => {
 
   let fnInterval = () => {
     currentImage++
-    console.log(currentImage, images.length)
     if(currentImage < images.length) {
       images[currentImage].style.opacity = '1'
     }
@@ -84,3 +83,32 @@ const bannersAnimation = () => {
   let interval = setInterval(fnInterval, 5000)
   
 }
+
+// Temporal
+const page = document.getElementById('page')
+const fullBox = document.getElementById('full-box')
+fullBox.style.display = 'none'
+page.style.display = 'block'
+bannersAnimation()
+// Temporal
+
+const checkZone = () => {
+  const viewportHeight = window.innerHeight
+  const scrollTop = window.scrollY
+  const contactButton = document.querySelector('#page .contact')
+
+  if(scrollTop > viewportHeight) { contactButton.classList.add('black') }
+  else { contactButton.classList.remove('black') }
+}
+
+checkZone()
+
+window.addEventListener('scroll', e => {
+  checkZone()
+})
+
+const arrowDown = document.querySelector('#arrow-down')
+arrowDown.addEventListener('click', () => {
+  const positionAboutUs = document.querySelector('#ladrillo').offsetTop
+  window.scroll(0, positionAboutUs)
+})
